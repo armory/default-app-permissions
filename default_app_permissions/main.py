26f0f97c07816194f37a4a6a7d97fa36c0facec6
Isaac Mosquera <imosquer@gmail.com>
2017-08-29 09:44:47 -0700

    this is now configured

--- default_app_permissions/main.py
+++ default_app_permissions/main.py
@@ -7,17 +7,18 @@ default-app-permissions [PROTOCOL://HOST:PORT] [SESSION_ID] [GROUP_NAME] [EMAIL]
 """
 
 def get_apps(make_req):
-    response = make_req("get", "/applications")
+    response = make_req("get", "applications")
     return response
 
 def register_app(make_req, email, group_name):
     def _register_app(app):
-        path = "/application/%s/tasks" % app["name"]
         app_json = {
             "application": app["name"],
+            "description": "Setting Default Permissions on Application: %s" % app["name"],
             "job":[{
                 "type":"updateApplication",
                 "application": {
+                    "name": app["name"],
                     "email": email,
                     "permissions":{
                         "READ":[ group_name ],
@@ -26,17 +27,30 @@ def register_app(make_req, email, group_name):
                 },
             }]
         }
-        return make_req(post, path, json.dumps(app_json))
+        path = "applications/%s/tasks" % app["name"]
+        return make_req("post", path, data=json.dumps(app_json))
     return _register_app
 
-    """
-    {"job":[{"type":"updateApplication","application":{"cloudProviders":"gce","name":"gke","instancePort":null,"dataSources":{"enabled":[],"disabled":[]},"email":"isaac@armory.io","permissions":{"READ":["core"],"WRITE":["core"]}},"user":"imosquera"}],"application":"gke","description":"Update Application: gke"}
-    """
 def make_request(host, session_id):
     def _make_request(method, path, data=None):
         request_method = getattr(requests, method)
-        response = request_method("%s/%s" % (host, path), cookies={'SESSION':session_id}, data=data)
-        resp_json = json.loads(response.text)
+        full_url = "%s/%s" % (host, path)
+        print("calling url: %s" % full_url)
+        response = request_method(full_url,
+            cookies={'SESSION':session_id},
+            data=data,
+            headers={'content-type': 'application/json'}
+        )
+        print("status code: %s for %s" % (response.status_code, full_url))
+        if response.status_code >= 300  or response.status_code < 200:
+            print(response.text)
+            print("something went terribly wrong!")
+            sys.exit(1)
+        try:
+            resp_json = json.loads(response.text)
+        except Exception    as e:
+            print("couldn't parse json \n %s" % response.text)
+            raise e
         return resp_json
     return _make_request
 
@@ -54,8 +68,12 @@ def main():
     print("Adding Group Name:%s" % group_name)
     request_fn = make_request(host, session_id)
     apps = get_apps(request_fn)
-    unconfigured_apps = filter(lambda x: "createTs" not in x, apps)
-    unconfigured_apps = filter(lambda x: x["name"] == "gke", unconfigured_apps)
-    registered_app = map(register_app(make_req, email, group_name), unconfigured_apps)
-    print(apps)
-    print(register_app)
+
+    unconfigured_apps = list(filter(lambda x: "createTs" not in x, apps))
+
+    print("-------------------- unconfigured_apps")
+    print(unconfigured_apps)
+
+    registered_app = map(register_app(request_fn, email, group_name), unconfigured_apps)
+    print("---------- registered")
+    print(list(registered_app))
